cfbab2543943b30161bfe91699d31efb012f083f
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2015-09-22 00:27:32 +0800

    Initial commit

---
+++ app.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+    // "encoding/json"
+    "log"
+    "fmt"
+    "strings"
+    // "regexp"
+    // "reflect"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "github.com/gin-gonic/gin"
+    // "thoughtmonkeys.com/linklist/models"
+)
+
+
+
+func main() {
+
+    // Connect to DB
+    db, err := sql.Open("postgres", "postgres://linklist:linklist@localhost/linklist")
+    if err != nil {
+        fmt.Printf("[DB] Error: %s", err)
+        log.Fatal(err)
+    }
+    defer db.Close()
+    fmt.Printf("Created DB: %s", db)
+
+    // Create a table
+    if _, err = db.Exec("CREATE TABLE IF NOT EXISTS links ( id SERIAL PRIMARY KEY, person jsonb, url jsonb, created timestamp with time zone, chat integer, tags jsonb )"); err != nil {
+        log.Fatal(err)
+    }
+
+    // Supports queries like:
+    // SELECT data->'url' FROM links WHERE data @> '{"domain": "github.com"}'
+    if rows, err := db.Query("SELECT to_regclass('links.idxdomains')"); rows == nil {
+        if _, err = db.Exec("CREATE INDEX idxdomains ON links USING gin (url jsonb_path_ops)"); err != nil {
+            log.Fatal(err)
+        }
+    }
+    // Supports queries like:
+    // SELECT data->'url' FROM links WHERE data -> 'tags' ? 'kafka'
+    // TODO: Implement tags
+    // if _, err = db.Exec("CREATE UNIQUE INDEX idxtags ON links USING gin ((data -> 'tags'))"); err != nil {
+    //     log.Fatal(err)
+    // }
+
+
+    router := gin.Default()
+
+    router.POST("/save", func(c *gin.Context) {
+
+        update := Update{}
+        fmt.Println("pre-bind")
+        e := c.BindJSON(&update)
+        if e != nil {
+            log.Fatal(e)
+        }
+
+        fmt.Println(update.Message.Date)
+
+        // update.Message.URL.Parse()
+        if strings.HasPrefix(update.Message.Text, "/save") {
+
+            url := URL{}
+            _, err = url.Extract(update.Message.Text)
+            if err != nil {
+                log.Fatal(err)
+            }
+            url.Parse()
+
+            // Insert into DB
+            // var data []byte
+            // if data, err = json.Marshal(url.URL); err != nil {
+            //     log.Fatal(err)
+            // }
+            // fmt.Println(string(data[:]))
+
+            fmt.Println(update.Message.Date)
+            fmt.Printf("url: %s | %s\n", url, Model{M: &url}.ToJSON())
+            fmt.Printf("person: %s | %s\n", update.Message.From, Model{M: &update.Message.From}.ToJSON())
+            if _, err = db.Exec("INSERT INTO links (person, url, created, chat) VALUES ($1, $2, $3, $4)", Model{M: &update.Message.From}.ToJSON(), Model{M: &url}.ToJSON(), update.Message.Date.Time(), update.Message.Chat.Id); err != nil {
+                log.Fatal(err)
+            }
+        }
+        // if _, err = db.Exec("INSERT INTO links (url) VALUES ($1)", data); err != nil {
+        //     fmt.Println(err)
+        //     log.Fatal(err)
+        // }
+
+
+        c.JSON(200, gin.H{"success": true})
+
+    })
+
+    // router.POST("/save", func(c *gin.Context) {
+
+    //     var url URL
+    //     e := c.BindJSON(&url)
+    //     if e == nil {
+
+    //         url.Parse()
+
+    //         // Insert into DB
+    //         var data []byte
+    //         if data, err = json.Marshal(url); err != nil {
+    //             log.Fatal(err)
+    //         }
+    //         fmt.Println(string(data[:]))
+    //         if _, err = db.Exec("INSERT INTO links (url) VALUES ($1)", data); err != nil {
+    //             fmt.Println(err)
+    //             log.Fatal(err)
+    //         }
+
+
+    //         c.JSON(200, url)
+    //     } else {
+    //         fmt.Println(e)
+    //     }
+
+    // })
+
+
+    router.POST("/message", func(c *gin.Context) {
+
+        var message Message
+        e := c.BindJSON(&message)
+        if e == nil {
+            c.JSON(200, gin.H{"from": message.From.Username})
+        } else {
+            fmt.Println(e)
+        }
+
+    })
+
+    router.POST("/user", func(c *gin.Context) {
+
+        var user User
+        e := c.BindJSON(&user)
+        if e == nil {
+            c.JSON(200, user)
+        } else {
+            fmt.Println(e)
+        }
+
+    })
+
+    router.GET("/:action/user", func(c *gin.Context) {
+
+        action := c.Params.ByName("action")
+        c.JSON(200, gin.H{"action": action, "end": "user"})
+    })
+
+    router.GET("/:action/duh", func(c *gin.Context) {
+
+        action := c.Params.ByName("action")
+        c.JSON(200, gin.H{"action": action, "end": "duh"})
+    })
+
+
+    // router.GET("/ping", func(c *gin.Context) {
+
+    //     c.String(200, "pong")
+    // })
+
+
+
+    router.Run("0.0.0.0:8000")
+
+}
---
+++ models.go
@@ -0,0 +1,142 @@
+
+package main
+
+import (
+    "errors"
+    "encoding/json"
+    "fmt"
+    "log"
+    "regexp"
+    "reflect"
+    "strconv"
+    "time"
+)
+
+// type Model interface {
+//     ToJSON() string
+// }
+
+type Model struct {
+    // Ref: http://dennissuratna.com/using-anonymous-field-to-extend-in-go/
+    M interface{}
+}
+
+func (m Model) ToJSON() string {
+    // var data []byte
+    fmt.Printf("Model ToJSON: %s\n", m.M)
+    data, err := json.Marshal(m.M)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return string(data[:])
+}
+
+// Ref: https://medium.com/coding-and-deploying-in-the-cloud/time-stamps-in-golang-abcaf581b72f
+type Timestamp time.Time
+
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+    ts := time.Time(*t).Unix()
+    stamp := fmt.Sprint(ts)
+
+    return []byte(stamp), nil
+}
+
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+    ts, err := strconv.Atoi(string(b))
+    if err != nil {
+        return err
+    }
+
+    *t = Timestamp(time.Unix(int64(ts), 0))
+
+    return nil
+}
+
+func (t *Timestamp) Time() time.Time {
+    return time.Time(*t)
+}
+
+type Update struct {
+    // Model
+    UpdateId     int      `json:"update_id"`
+    Message      Message  `json:"message"`
+}
+
+type User struct {
+    // Model
+    Id           int      `json:"id"`
+    FirstName    string   `json:"first_name"`
+    LastName     string   `json:"last_name"`
+    Username     string   `json:"username"`
+}
+
+type Message struct {
+    // Model
+    MessageId    int        `json:"message_id"`
+    From         User       `json:"from"`
+    Date         Timestamp  `json:"date,float64"`
+    Chat         Chat       `json:"chat"`
+    Text         string     `json:"text"`
+}
+
+type Chat struct {
+    // Model
+    Id        int       `json:"id"`
+}
+// type Chat     int       `json:"id"`
+
+type Link struct {
+    // Model
+    Id        int       `json:"id"`
+    User      User      `json:"user"`
+    URL       URL       `json:"url"`
+    Created   Timestamp `json:"created"`
+    Tags      []string  `json:"tags"`
+}
+
+type URL struct {
+    // Model
+    URL       string    `json:"url"`
+    Domain    string    `json:"domain"`
+    Path      string    `json:"path"`
+    Scheme    string    `json:"scheme"`
+    Hash      string    `json:"hash"`
+    Search    string    `json:"search"`
+}
+
+func (url *URL) Parse () *URL {
+
+    // Receiver is pointer, therefor is *address* of actual URL instance
+
+    var URLRE = regexp.MustCompile(`^(?P<Scheme>https?):\/\/(?P<Domain>[^/?]+)(\/(?P<Path>[^#?]+))?/?(\?(?P<Search>[^#]+))?(#(?P<Hash>.+))?$`)
+
+    match := URLRE.FindStringSubmatch(url.URL)
+    if len(match) > 0 {
+        for i, name := range URLRE.SubexpNames() {
+            if name == "" || match[i] == "" {
+                continue
+            }
+
+            // Where we'd normally pass &url into reflect.ValueOf(),, since
+            // this method's receiver is a pointer to an URL, ie. *URL, we
+            // can simply pass that parameter into reflect.ValueOf()
+            reflect.ValueOf(url).Elem().FieldByName(name).SetString(match[i])
+        }
+    }
+
+    return url
+}
+
+func (url *URL) Extract (s string) (string, error) {
+
+    var URLEXT = regexp.MustCompile(`https?:\/\/[\w\-.?#/]+`)
+    match := URLEXT.FindStringSubmatch(s)
+    if len(match) == 0 {
+        return "", errors.New("No match")
+    } else {
+        url.URL = match[0]  // match[0] is the original string
+        return match[0], nil
+    }
+
+}
